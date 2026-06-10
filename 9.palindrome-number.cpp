c1b7c042b6db93c31689794685d44af312b5e064
Ngô Viết Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-06-10 12:45:43 +0700

    Initial LeetCode solutions

---
+++ 9.palindrome-number.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        long reverse = 0;
+        int x_cop = x;
+        
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10 ;
+            x /= 10;
+        }
+
+        return reverse == x_cop;
+    }
+};
+// @lc code=end
+
---
+++ README.md
@@ -0,0 +1,17 @@
+# LeetCode
+
+Daily LeetCode practice solutions.
+
+## Structure
+
+- Each solution file is named with the LeetCode problem number and slug.
+- Example: `9.palindrome-number.cpp`
+
+## Daily Workflow
+
+```bash
+git status
+git add .
+git commit -m "Add solution for problem <number>"
+git push
+```
