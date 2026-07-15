c0ba6de423789aaaddf171b88cf3628f2263aa2c
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-07-15 15:14:28 +0700

    07-15-2026 Easy 0 ms (100%) 8.6 MB (9.11%)

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -3,6 +3,6 @@ public:
     int gcdOfOddEvenSums(int n) {
         // sumOdd = 1 + 3 + 5 + ... + (2n-1) = n^2
         // sumEven= 2 + 4 + 6 + ... + 2n     = n(n-1)
-        return gcd(n*n, n*(n-1));
+        return n;
     }
 };
