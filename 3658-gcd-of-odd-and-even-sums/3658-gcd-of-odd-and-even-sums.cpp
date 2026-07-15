b4acef18ece30ff3d7adeea0e2f23a3765779d25
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-07-15 15:03:36 +0700

    07-15-2026 Easy 0 ms (100%) 8.6 MB (9.11%)

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = 0;
+        int sumEven = 0;
+        for(int i = 1; i <= 2*n; i++){
+            if(i % 2 == 0) sumEven += i;
+            else sumOdd += i;
+        }
+        return gcd(sumOdd, sumEven);
+    }
+};
