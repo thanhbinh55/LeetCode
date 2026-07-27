05a75f79d5a20798ae4ede9c8814820892769bf4
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-07-27 20:56:40 +0700

    07-27-2026 Easy 3 ms (20.55%) 13.5 MB (25.65%)

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            nums[i] = nums[i] - 1;
+        }
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() - 1] * nums[nums.size() - 2];
+    }
+};
