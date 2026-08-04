9ac939278d0e3cac62dd5457abc61d82b996d0dd
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-08-04 23:45:27 +0700

    08-04-2026 Easy 7 ms (45.18%) 53.1 MB (76.85%)

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
 
-        for (int i = 0; i < nums.size(); i++) {
-            int idx = abs(nums[i]) - 1;
-            if (nums[idx] > 0)
-                nums[idx] = -nums[idx];
-        }
-
-        vector<int> ans;
+        vector<bool> check(nums.size(), false);
 
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0)
-                ans.push_back(i + 1);
+            check[nums[i] - 1] = true;
         }
 
-        return ans;
+        vector<int> missing;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (!check[i]) {
+                missing.push_back(i + 1);
+            }
+        }
+
+        return missing;
     }
 };
