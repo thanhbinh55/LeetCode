5102bbc36effd787d3e824b7ab0dec8a51ffdb66
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-09-15 23:38:46 +0700

    09-15-2026 Easy 0 ms (100%) 15 MB (28.76%)

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        vector<pair<int, int>> b;
+        unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++){
-            b.push_back({nums[i], i});
-        }
-        sort(b.begin(), b.end());
-
-        for(int i = 0; i < nums.size(); i++){
-            int need = target - b[i].first;
-
-            auto l = lower_bound(
-                b.begin(),
-                b.end(),
-                make_pair(need, -1)
-            );
-           
-            if(l != b.end() && l->first == need && l->second != b[i].second){
-                return {b[i].second, l->second};
+            int need = target - nums[i];
+            
+            auto it = mp.find(need);
+            if(it != mp.end()){
+                return {it->second, i};
             }
-        } 
 
+            mp[nums[i]] = i;
+        }
         return {};
     }
 };
