fc72ee5da2d7551af1c35aaf4a95b24528ebd4ca
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-09-15 23:00:45 +0700

    09-15-2026 Easy 0 ms (100%) 14.9 MB (44.71%)

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,31 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        for(int i = 0; i < nums.size() - 1; i++){
-            for(int j = i+1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
+        vector<pair<int, int>> b;
+        for(int i = 0; i < nums.size(); i++){
+            b.push_back({nums[i], i});
+        }
+        sort(b.begin(), b.end());
+
+        for(int i = 0; i < nums.size(); i++){
+            int need = target - b[i].first;
+
+            auto l = lower_bound(
+                b.begin(),
+                b.end(),
+                make_pair(need, -1)
+            );
+
+            for(auto p = l; p != b.end() && p->first == need; p++){
+                if(p->second != b[i].second){
+                     ans.push_back(b[i].second);
+                    ans.push_back(p->second);
                     break;
                 }
             }
-        }
+            if (!ans.empty()){ break;}
+        } 
+
         return ans;
     }
 };
