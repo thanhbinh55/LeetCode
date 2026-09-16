e4d6fc7f868c444d8b33d789e558ffbf40b426c9
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-09-16 23:57:23 +0700

    09-16-2026 Easy 0 ms (100%) 24.6 MB (50.62%)

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
         // solution 1: 2 for loop
-        int count = 0;
-        for(int i = 0; i < nums.size() - 1; i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                int sum = nums[i] + nums[j];
-                if(sum < target){
-                    count ++;
-                }
+        // solution 2: sort + two pointers
+        int L = 0;
+        int R = nums.size() - 1;
+        long long ans = 0;
+        sort(nums.begin(), nums.end());
+        while(L < R) {
+            if(nums[L] + nums[R] < target){
+                ans += R - L;
+                L++;
+            }else{
+                R--;
             }
         }
-        return count;
+        return ans;
     }
 };
