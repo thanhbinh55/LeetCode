6b7a5aab3c21c1ca7a6ae87948801a22c9f949cc
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-09-22 16:56:47 +0700

    09-22-2026 Medium 0 ms (100%) 71.8 MB (51.96%)

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int current = nums[0];
-        int ans = nums[0];
+        long long current_prefix = 0;
+        long long min_prefix = 0; // prefix_sum[-1] = 0
+        long long max_so_far = nums[0];
+        
+        for(int x: nums){
+            current_prefix += x;
 
-        for(int i = 1; i < nums.size(); i++){
-            current = max(nums[i], current + nums[i]);
-            ans = max(ans, current);
+            max_so_far = max(max_so_far, current_prefix - min_prefix);
+
+            min_prefix = min(min_prefix, current_prefix);
         }
         
-        return ans;
+        return (int)(max_so_far);
     }
 };
