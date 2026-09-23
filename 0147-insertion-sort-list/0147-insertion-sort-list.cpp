d25099a11b18a295000e6a083ae14c60676a16ef
Thanh Bình <ngovietthanhbinh2006@gmail.com>
2026-09-23 23:57:16 +0700

    09-23-2026 Medium 1 ms (91.14%) 14.6 MB (79.76%)

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   
+    ListNode* insertionSortList(ListNode* head) {
+       if(!head || !head->next) return head;
+
+       ListNode dummy(0);
+       dummy.next = head;
+       ListNode* lastSorted = head;
+       ListNode* cur = head->next;
+
+       while(cur){
+        if(cur->val >= lastSorted->val){
+            lastSorted = lastSorted->next;
+        }else{
+            ListNode* prev = &dummy;
+            while(prev->next->val < cur->val){
+                prev = prev->next;
+            }
+
+            lastSorted->next = cur->next;
+            cur->next = prev->next;
+            prev->next = cur;
+        }
+        cur = lastSorted->next;
+       }
+       return dummy.next;
+    }
+    
+
+};
